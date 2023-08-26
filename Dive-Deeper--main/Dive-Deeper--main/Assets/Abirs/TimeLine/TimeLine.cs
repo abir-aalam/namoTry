b1d22b53a5a19e6062baedda8ba3648880c1b2aa
abir ansari <abiransari122@gmail.com>
2023-08-26 15:42:20 +0530

    TimeLineChanges

--- Dive-Deeper--main/Dive-Deeper--main/Assets/Abirs/TimeLine/TimeLine.cs
+++ Dive-Deeper--main/Dive-Deeper--main/Assets/Abirs/TimeLine/TimeLine.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Timeline;
+using UnityEngine.SceneManagement;
 
 public class TimeLine : MonoBehaviour
 {
     Volume PostVolume;
+    Bloom b;
     public bool PlayPost = false;
+    public int sceneIndex;
     // Start is called before the first frame update
     void Start()
     {
         PostVolume = GetComponent<Volume>();
+        PostVolume.profile.TryGet(out b);
     }
 
     // Update is called once per frame
@@ -20,11 +24,15 @@ public class TimeLine : MonoBehaviour
     {
         if(PlayPost)
         {
-
+            b.intensity.value += 1000f * Time.deltaTime;
         }
     }
     public void PlayPos()
     {
         PlayPost = true;
+    }
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 }
